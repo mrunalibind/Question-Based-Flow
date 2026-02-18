df512a4ce9b175cb534ffb1c964ca46418808ac1
Mrunali Bind <mrunalibind123@gmail.com>
2026-02-18 22:12:49 +0530

    Configuration

--- src/config/database.js
+++ src/config/database.js
@@ -2,42 +2,16 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-// let obj = {
-//     DB_NAME: process.env.DB_NAME,
-//     DB_USER: process.env.DB_USER,
-//     DB_PASSWORD: process.env.DB_PASSWORD,   
-//     DB_HOST: process.env.DB_HOST,
-//     DB_PORT: process.env.DB_PORT
-// };
-// console.log(obj);
 
-// const sequelize = new Sequelize(process.env.DB_URI, {
-//   dialect: "mysql",
-// });
-// console.log("Database URI:", process.env.DB_URI);
-
-const sequelize = new Sequelize(process.env.DB_URI, {
-  dialect: "mysql",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",    
     }
-  }
-});
-
-// const sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     process.env.DB_URI ? {
-//     {
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         dialect: "mysql",
-//     }
-// );
-
-
+);
 
 export default sequelize;
