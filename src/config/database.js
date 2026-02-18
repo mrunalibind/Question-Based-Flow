afbc40e987271138315fc8e3bb5f81ee5d4ff2f0
Mrunali Bind <mrunalibind123@gmail.com>
2026-02-18 20:56:55 +0530

    Configuration

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,15 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
+let obj = {
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,   
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT
+};
+console.log(obj);
+
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -13,5 +22,7 @@ const sequelize = new Sequelize(
         dialect: "mysql",
     }
 );
+
+
 
 export default sequelize;
